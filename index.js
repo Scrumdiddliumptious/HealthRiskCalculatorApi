ced2a3c2b0c5fc1aa4bc15c73822eaff1d584663
Collin Koldoff <collinkoldoff@gmail.com>
2023-02-20 08:50:41 -0600

    Initial API setup

--- index.js
+++ index.js
@@ -1,28 +1,38 @@
 const express = require('express')
+const bodyParser = require('body-parser').json()
 const open = require('open')
 app = express()
 
-var url = require('url');
-
 const port = process.env.PORT || 3000
-const majorVersion = 1
-const minorVersion = 2
-
-// Use Express to publish static HTML, CSS, and JavaScript files that run in the browser. 
-app.use(express.static(__dirname + '/static'))
 
 // The app.get functions below are being processed in Node.js running on the server.
 // Implement a custom About page.
-app.get('/about', (request, response) => {
-	console.log('Calling "/about" on the Node.js server.')
+app.get('/ping', (request, response) => {
+	console.log('Ping recieved')
 	response.type('text/plain')
-	response.send('About Node.js on Azure Template.')
+	response.send('Pong!')
 })
 
-app.get('/version', (request, response) => {
-	console.log('Calling "/version" on the Node.js server.')
-	response.type('text/plain')
-	response.send('Version: '+majorVersion+'.'+minorVersion)
+app.post('/calculate_risk', bodyParser, (request, response) => {
+  risk = {}
+
+  totalScore = request.body.age + request.body.bmi + request.body.bp + request.body.disease;
+  risk.score = totalScore;
+
+  if (totalScore <= 20) {
+    risk.risk = "Low Risk";
+  } else if (totalScore <= 50) {
+    risk.risk = "Moderate Risk";
+  } else if (totalScore <= 75) {
+    risk.risk = "High Risk";
+  } else {
+    risk.risk = "Uninsurable";
+  }
+
+  output.push(risk);
+
+	response.type('application/json')
+	response.send(risk)
 })
 
 // Custom 404 page.
@@ -42,6 +52,5 @@ app.use((err, request, response, next) => {
 
 app.listen(port, () => console.log(
   `Express started at \"http://localhost:${port}\"\n` +
-  `press Ctrl-C to terminate.`),
-  open(`http://localhost:${port}`)
+  `press Ctrl-C to terminate.`)
 )
