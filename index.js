0edff4fe5e70bd8b9551d244f4635ae5c132c641
Collin Koldoff <collinkoldoff@gmail.com>
2023-03-01 11:22:12 -0600

    Removed unused dependencies

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser").json();
 const cors = require("cors");
-const open = require("open");
 app = express();
 
 const port = process.env.PORT || 3000;
